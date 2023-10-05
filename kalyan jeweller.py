d362eac53219d9558e249bf021e02fc2988059f3
shriram1312 <146513920+shriram1312@users.noreply.github.com>
2023-10-05 18:35:48 +0530

    kalyan jeweller.py

--- kalyan jeweller.py
+++ kalyan jeweller.py
@@ -4,16 +4,16 @@ print("-:  WELLCOM TO KALYAN JWELLERS :-")
 
 print("-----------------------------------")
 
-name=input("Enter your name:-")
-gender=(input("Enter  'F' or  'M':-")).upper()
-age=int(input("Enter your age:-"))
+name=input("Enter your name:-")  # it will take input from user
+gender=(input("Enter  'F' or  'M':-")).upper()  # will take gender of user
+age=int(input("Enter your age:-")) # will take age of the user
 
 
-A=0
+grandtotal=0 
 
-status= True
+status= True  #initilization for entry level conditional statement
 
-while status:
+while status: # creating while condition loop which will execute till the  current status is True 
 
 
     print("---------------------------\n")
@@ -26,15 +26,15 @@ while status:
     print("---------------------------")
 
 
-    totalgoldrate=grm*5752                                    #total gold rate
+    totalgoldrate=grm*5752          #will give the value of grm*current gold rate to totalgoldrate
 
-    print("TOTAL GOLD RATE  :",totalgoldrate)
+    print("TOTAL GOLD RATE  :",totalgoldrate) # print total gold rate
     print("\n")
     print("MAKING CHARGES(1 GRM) : 845")
     print("-----------------------------")
 
-    makingcharge=grm*845                                      # total makimng charges of the product
-    totalamount=totalgoldrate+makingcharge                    # total amount of the product
+    makingcharge=grm*845                       # total makimng charges of the product
+    totalamount=totalgoldrate+makingcharge     # total amount of the product
 
     print("your total making charge of your product:",makingcharge)
     print("--------------------------------")
@@ -43,47 +43,92 @@ while status:
 
 
 
-    if age>65 and gender=='M':                                                        # nested conditional statements
-        if totalamount>=200000 and totalamount<=300000:
-            dis=totalamount*20/100
+    if age>65 and gender=='M':            # nested conditional statements
+        if totalamount>=200000 and totalamount<=300000: # if totalamoutnt is between 200000 to 300000 then
+            dis=totalamount*20/100 # it will give 20% discount
             print("you will get 20% discount on your purches\n")
        
    
-        elif totalamount>=300000  and  totalamount<=500000:
-            dis=totalamount*30/100
+        elif totalamount>=300000  and  totalamount<=500000: # if totalamoutnt is between 300000 to 500000 then
+            dis=totalamount*30/100    # it will give 30% discount
             print("you will get 30% discount your purches\n")
        
    
-        elif totalamount>=500000:
-            dis=totalamount*35/100
+        elif totalamount>=500000:  # if totalamount is greater then 500000 then 
+            dis=totalamount*35/100 # it will give 35% discount 
             print("you will get 35% discount your purches\n")
        
    
-        elif totalamount<=200000:
-            print("you will not get any discount\n")
+        elif totalamount<=200000:# if total amount is under 200000 then
+            print("you will not get any discount\n") # will not give any dicount
        
         else:
             print("you will not get any discount")
 
 
     elif age<65 and gender=='M':
-        if totalamount>=200000 and totalamount<=300000:
-            dis=totalamount*10/100
+        if totalamount>=200000 and totalamount<=300000:# if totalamoutnt is between 200000 to 300000 then
+            dis=totalamount*10/100 # it will give 10% discount
             print("you will get 10% discount your purches\n")
        
    
-        elif totalamount>=300000  and  totalamount<=500000:
-            dis=totalamount*20/100
+        elif totalamount>=300000  and  totalamount<=500000:  # if totalamoutnt is between 300000 to 500000 then
+            dis=totalamount*20/100 # it will give 20% discount
             print("you will get 20% discount your purches\n")
         
    
-        elif totalamount>=500000:
-            dis=totalamount*25/100
+        elif totalamount>=500000:  # if totalamount is greater then 500000 then
+            dis=totalamount*25/100 # it will give 25% discount
             print("you will get 25% discount your purches\n")
        
    
-        elif totalamount<=200000:
-            print("you will not get any discount\n")
+        elif totalamount<=200000: # if total amount is under 200000 the
+            print("you will not get any discount\n") # will not give any dicount
+       
+        else:
+            print(" ")
+
+    elif age>65 and gender=='F':             # nested conditional statements
+        if totalamount>=200000 and totalamount<=300000:# if totalamoutnt is between 200000 to 300000 then
+            dis=totalamount*25/100 # it will give 25% discount
+            print("you will get 25% discount on your purches\n")
+       
+   
+        elif totalamount>=300000  and  totalamount<=500000: # if totalamoutnt is between 300000 to 500000 then
+            dis=totalamount*35/100 # it will give 35% discount
+            print("you will get 35% discount your purches\n")
+       
+   
+        elif totalamount>=500000:  # if totalamount is greater then 500000 then
+            dis=totalamount*40/100 # it will give 40% discount
+            print("you will get 40% discount your purches\n")
+       
+   
+        elif totalamount<=200000: # if total amount is under 200000 the
+            print("you will not get any discount\n") # will not give any dicount
+       
+        else:
+            print("you will not get any discount")
+
+
+    elif age<65 and gender=='F':
+        if totalamount>=200000 and totalamount<=300000:# if totalamoutnt is between 200000 to 300000 then
+            dis=totalamount*15/100 # it will give 15% discount
+            print("you will get 15% discount your purches\n")
+       
+   
+        elif totalamount>=300000  and  totalamount<=500000: # if totalamoutnt is between 300000 to 500000 then
+            dis=totalamount*25/100 # it will give 25% discount
+            print("you will get 25% discount your purches\n")
+        
+   
+        elif totalamount>=500000:  # if totalamount is greater then 500000 then
+            dis=totalamount*30/100 # it will give 30% discount
+            print("you will get 30% discount your purches\n")
+       
+   
+        elif totalamount<=200000: # if total amount is under 200000 the
+            print("you will not get any discount\n") # will not give any dicount
        
         else:
             print(" ")
@@ -93,19 +138,19 @@ while status:
         print("you have not give proper information")
 
 
-    disamount=dis-makingcharge
+    disamount=dis-makingcharge # it will show discount amount without makinng charges 
 
 
-    if disamount<0:
-        disamount="zero"
+    if disamount<0: # if discount amount is less then 0 
+        disamount="zero" # assign zero to discount amount 
         print(f"your dicounted amount without making charges: {disamount}")
 
-    else:
-        disamount=disamount
+    else:  
+        disamount=disamount # it will assign original value to discount amount
         print(f"your dicounted amount without making charges: {disamount}")
 
     print("---------------------------------")
-    totalamount1=totalamount-dis
+    totalamount1=totalamount-dis # store the value of final amount into totalamount1
     print("FINAL amount :",totalamount1)
     print("\n")
 
@@ -113,16 +158,16 @@ while status:
     choice=input("do you wnat to purchese more: press 'Y' for yes and press 'N' for no:").upper()
 
     if choice=='y' or choice=='Y' or choice=='yes':
-        status=True
+        status=True   # if status= true, then it will run while loop again
     else:
        
-       status=False
+       status=False # it will break the loop 
 
     
-    A+=totalamount1
+    grandtotal+=totalamount1 # grand total of all products
 
     
-print("your grand total of all product:",A)
+print("your grand total of all product:",grandtotal) #it will give total of all products 
 print("\n")
 print("-:THANKU FOR VISITING KALYAN JWELLER:-")
     
